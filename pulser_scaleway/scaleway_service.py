07b6c8537f491d072083d3dba3550f5438ee9ba0
Valentin Macheret <vmacheret@scaleway.com>
2025-08-06 11:58:48 +0200

    feat(scw): implem pulser client

--- pulser_scaleway/scaleway_service.py
+++ pulser_scaleway/scaleway_service.py
@@ -12,8 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.from typing import Optional, List, Dict
 import os
+import typing
+import httpx
+import json
 
-from typing import Optional, Any, Mapping, Type, TypedDict
+from typing import Optional, Mapping, List, Dict, Tuple
 
 from pulser import Sequence
 from pulser.backend.config import EmulatorConfig
@@ -24,14 +27,13 @@ from pulser.backend.remote import (
     JobStatus,
     RemoteConnection,
     RemoteResults,
-    RemoteResultsError,
 )
 from pulser.json.utils import make_json_compatible
 from pulser.backend.abc import Backend
 from pulser.backend.results import Results, ResultsSequence
 from pulser.devices import Device
 
-from scaleway_qaas_client.v1alpha1 import QaaSClient, QaaSPlatform
+from scaleway_qaas_client.v1alpha1 import QaaSClient, QaaSPlatform, QaaSJobResult
 
 
 class ScalewayQuantumService(RemoteConnection):
@@ -45,7 +47,7 @@ class ScalewayQuantumService(RemoteConnection):
         project_id = project_id or os.getenv("PULSER_SCALEWAY_PROJECT_ID")
         url = url or os.getenv("PULSER_SCALEWAY_API_URL")
 
-        pass
+        self._client = QaaSClient(project_id=project_id, secret_key=secret_key, url=url)
 
     def submit(
         self,
@@ -53,7 +55,7 @@ class ScalewayQuantumService(RemoteConnection):
         wait: bool = False,
         open: bool = False,
         batch_id: str | None = None,
-        **kwargs: Any,
+        **kwargs,
     ) -> RemoteResults:
 
         sequence = self._add_measurement_to_sequence(sequence)
@@ -73,15 +75,45 @@ class ScalewayQuantumService(RemoteConnection):
 
         payload = sequence.to_abstract_repr()
 
+    def _get_data_from_job_result(self, job_result: QaaSJobResult) -> str:
+        result = job_result.result
+
+        if result is None or result == "":
+            url = job_result.url
+
+            if url is not None:
+                resp = httpx.get(url.replace("http://s3", "http://localhost"))
+                resp.raise_for_status()
+
+                return resp.text
+            else:
+                raise Exception("Got result with empty data and url fields")
+        else:
+            return result
+
+    def _get_result(self, job_id: str) -> Results:
+        job_results = self._client.list_job_results(job_id=job_id)
+
+        if job_results is None or len(job_results) == 0:
+            return None
+
+        job_result = self._get_data_from_job_result(job_results[0])
+
+        return Results.from_abstract_repr(json.loads(job_result))
+
     def _fetch_result(
-        self, batch_id: str, job_ids: list[str] | None
-    ) -> Sequence[Results]:
+        self, batch_id: str, job_ids: List[str] | None
+    ) -> typing.Sequence[Results]:
         """Fetches the results of a completed batch."""
-        pass
+        jobs = self._client.list_jobs(session_id=batch_id)
+
+        jobs_results = [self._get_result(job.id) for job in jobs]
+
+        return jobs_results
 
     def _query_job_progress(
         self, batch_id: str
-    ) -> Mapping[str, tuple[JobStatus, Results | None]]:
+    ) -> Mapping[str, Tuple[JobStatus, Results | None]]:
         """Fetches the status and results of all the jobs in a batch.
 
         Unlike `_fetch_result`, this method does not raise an error if some
@@ -89,24 +121,71 @@ class ScalewayQuantumService(RemoteConnection):
 
         It returns a dictionary mapping the job ID to its status and results.
         """
-        pass
+        jobs = self._client.list_jobs(session_id=batch_id)
+
+        status_mapping = {
+            "waiting": JobStatus.PENDING,
+            "running": JobStatus.RUNNING,
+            "completed": JobStatus.DONE,
+            "cancelled": JobStatus.CANCELED,
+            "cancelling": JobStatus.CANCELED,
+        }
+
+        job_progress = {
+            job.id: (
+                status_mapping.get(job.status, JobStatus.ERROR),
+                (
+                    self._get_result(job.id)
+                    if status_mapping.get(job.status, JobStatus.ERROR) == JobStatus.DONE
+                    else None
+                ),
+            )
+            for job in jobs
+        }
+
+        return job_progress
 
     def _get_batch_status(self, batch_id: str) -> BatchStatus:
         """Gets the status of a batch from its ID."""
-        pass
+        session = self._client.get_session(session_id=batch_id)
 
-    def _get_job_ids(self, batch_id: str) -> list[str]:
+        status_mapping = {
+            "starting": BatchStatus.PENDING,
+            "running": BatchStatus.RUNNING,
+            "stopping": BatchStatus.CANCELED,
+            "stopped": BatchStatus.DONE,
+        }
+
+        return status_mapping.get(session.status, BatchStatus.ERROR)
+
+    def _get_job_ids(self, batch_id: str) -> List[str]:
         """Gets all the job IDs within a batch."""
-        return None
+        jobs = self._client.list_jobs(session_id=batch_id)
+        job_ids = [job.id for job in jobs]
 
-    def fetch_available_devices(self) -> dict[str, Device]:
+        return job_ids
+
+    def fetch_available_devices(self) -> Dict[str, Device]:
         """Fetches the devices available through this connection."""
-        devices = {}
+        platforms = self._client.list_platforms(provider_name="pasqal")
+
+        def _plt_to_device(plt: QaaSPlatform) -> Device:
+            metadata = plt.metadata
+
+            return Device(
+                name=plt.name,
+                max_runs=plt.max_shot_count,
+                max_atom_num=plt.max_qubit_count,
+                **metadata,
+            )
+
+        devices = {plt.name: _plt_to_device(plt) for plt in platforms}
+
         return devices
 
     def _close_batch(self, batch_id: str) -> None:
         """Closes a batch using its ID."""
-        return None
+        self._client.terminate_session(session_id=batch_id)
 
     def supports_open_batch(self) -> bool:
         """Flag to confirm this class can support creating an open batch."""
